dd874a1d7f2956ceec8c8a3d851eb523401cfe7c
Zerui-WANG <71635569+Zerui-WANG@users.noreply.github.com>
2021-01-09 16:32:39 +0100

    paiement

--- src/application/panierController.java
+++ src/application/panierController.java
@@ -21,85 +21,90 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.stage.Window;
 import javafx.util.converter.NumberStringConverter;
 
-public class panierController implements Initializable{
-	
+public class panierController implements Initializable {
+
 	Popup popup;
 	Popup popupQuantNull;
 	Label message;
-	@FXML 
-	private TextField quantity_text;  
+	@FXML
+	private TextField quantity_text;
 	@FXML
 	private Button add;
 	private Button supp;
-	@FXML 
+	@FXML
 	private Button passerLaCom;
-	@FXML 
+	@FXML
 	private Label lbl_sous_total;
-	@FXML 
+	@FXML
 	private Label reduction;
-	@FXML 
+	@FXML
 	private Label lbl_total;
-	@FXML 
+	@FXML
 	private Text lbl_price;
-	@FXML 
+	@FXML
 	private Label lbl_total_commande;
-	@FXML 
-	public void addArticle(ActionEvent event)
-	{
-		Integer quantite = Integer.valueOf(quantity_text.getText()); 
+
+	@FXML
+	public void addArticle(ActionEvent event) {
+		Integer quantite = Integer.valueOf(quantity_text.getText());
 		Integer r = quantite + 1;
-		Float prix = Float.valueOf(lbl_price.getText()); 
+		Float prix = Float.valueOf(lbl_price.getText());
 		Float total = r * prix;
 		quantity_text.setText(r.toString());
 		lbl_total.setText(total.toString());
 		lbl_sous_total.setText(total.toString());
-		//mise à jour de la réduction quand il y a plus de 3 articles 
-		if (r >=3 )
-		{
+		// mise à jour de la réduction quand il y a plus de 3 articles
+		if (r >= 3) {
 			reduction.setText("15");
 		}
-		
-		Integer reduc = Integer.valueOf(reduction.getText()); 
+
+		Integer reduc = Integer.valueOf(reduction.getText());
 		if (reduc == 0) {
 			Float totalAPayer = total;
 			lbl_total_commande.setText(totalAPayer.toString());
-		}
-		else {
-			Float totalAPayer = (float) (total * 0.85) ;
+		} else {
+			Float totalAPayer = (float) (total * 0.85);
 			lbl_total_commande.setText(totalAPayer.toString());
 		}
 	}
-	public void deleteArticle (ActionEvent event) {
-		Integer quantite = Integer.valueOf(quantity_text.getText()); 
+
+	public void deleteArticle(ActionEvent event) {
+		Integer quantite = Integer.valueOf(quantity_text.getText());
 		Integer r = quantite - 1;
 		if (r >= 0) {
 			quantity_text.setText(r.toString());
-			
+
 		}
 	}
-	public void handlePopup(ActionEvent event){ 
-        if(popup.isShowing()){ 
-            popup.hide(); 
-            }else { 
-                final Window window = passerLaCom.getScene().getWindow(); 
-                popup.setWidth(50); 
-                popup.setHeight(100); 
-                final double x = window.getX() + passerLaCom.localToScene(0, 0).getX() + passerLaCom.getScene().getX() ; 
-                final double y = window.getY() + passerLaCom.localToScene(0, 0).getY() + passerLaCom.getScene().getY() + passerLaCom.getHeight(); 
-                popup.getContent().clear(); 
-                popup.getContent().addAll(message); 
-                popup.show(window, x, y);
-                } 
-        }
+
+	public void pressButton(ActionEvent event) throws Exception {
+		try {
+
+			Stage stage = new Stage();
+			Parent root = FXMLLoader.load(getClass().getResource("PaymentView.fxml"));
+			Scene scene = new Scene(root,1094,623);
+			stage.setTitle("Payment");
+			stage.setScene(scene);
+			stage.show();
+			
+			Stage precedentStage = (Stage) passerLaCom.getScene().getWindow();
+		    // do what you have to do
+			precedentStage.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		popup = new Popup(); 
-		message = new Label("Vous serez redigé vers le paiement ... "); 
+		popup = new Popup();
+		message = new Label("Vous serez redigé vers le paiement ... ");
 		message.setTextFill(Color.BLUE);
-		message.setPadding(new Insets(5,0,0,0));
-	}     
-	
+		message.setPadding(new Insets(5, 0, 0, 0));
+	}
+
 }
